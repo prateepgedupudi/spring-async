fb72d3a238f2d39f783597dc1a4bb46154baa912
Prateep Gedupudi <prateep.gedupudi@gmail.com>
2017-11-23 19:24:53 +0530

    Commit with basics.

---
+++ src/main/java/info/prateep/springasync/PollerListener.java
@@ -0,0 +1,26 @@
+package info.prateep.springasync;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.messaging.Message;
+
+import java.util.List;
+import java.util.Map;
+
+
+@MessageEndpoint
+public class PollerListener {
+    private static final Logger LOG = LoggerFactory.getLogger(PollerListener.class);
+    @ServiceActivator(inputChannel = "dataChannel")
+    public void process(Message<List<Map<String, Object>>> msg){
+        if(msg!= null && msg.getPayload()!= null){
+            LOG.info("Message: {}", msg);
+
+        }
+
+    }
+
+}
